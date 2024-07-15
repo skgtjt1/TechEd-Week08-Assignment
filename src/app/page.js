430df15577e08ccbcb020347689e9596ea4a345d
skgtjt1 <justin.tosh.14@ucl.ac.uk>
2024-07-15 23:09:24 +0100

    added title to header

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Link href="/expansions">
-        {" "}
         <Image
           className="logo"
           src="/assets/logo.svg"
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,10 @@ export default function Header() {
       {/* we add the module name (that we choose in the import) and the rules we want to apply */}
       {/* <h1 className={HeaderStyles.h1}>Testing Header</h1> */}
       {/* <nav className={HeaderStyles.nav}> */}
-
+      <h1 className="lg:text-xl font-extrabold  ">
+        An Unofficial FFXIV Expansion Review Site
+      </h1>
+      <br />
       <nav className={HeaderStyles.nav}>
         <ul className={HeaderStyles.list}>
           <li>
